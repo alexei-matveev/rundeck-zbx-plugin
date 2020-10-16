9ac059be30c96fce2abe51928e0ca012de4a74f3
Alexei Matveev <alexei.matveev@gmail.com>
2020-10-16 16:48:27 +0200

    AM: run query print hosts

--- src/rundeck_zbx_plugin/core.clj
+++ src/rundeck_zbx_plugin/core.clj
@@ -4,22 +4,34 @@
 ;; Rundeck. Maybe you should also implement a CLI ...
 ;;
 (ns rundeck-zbx-plugin.core
-  (:require [proto-zabbix.api :as api])
+  (:require [proto-zabbix.api :as api]
+            [clojure.edn :as edn]
+            [clojure.pprint :as pp])
   (:gen-class))
 
+(defn- make-host [zabbix-host]
+  (select-keys zabbix-host
+               [:host :name :description]))
 ;;
-;; Config should be like:
+;; Properties should supply at least these fields:
 ;;
-;;   {:url "https://zabbix.example.com/api_jsonrpc.php"
-;;    :user "user"
-;;    :password "password"}
+;;   {"url" "https://zabbix.example.com/api_jsonrpc.php"
+;;    "user" "user"
+;;    "password" "password"}
 ;;
 (defn query [properties]
   (let [config {:url (get properties "url")
                 :user (get properties "user")
                 :password (get properties "password")}
-        zbx (api/make-zbx config)]
-    (take 3 (zbx "host.get"))))
+        zbx (api/make-zbx config)
+        zabbix-hosts (zbx "host.get")
+        hosts (map make-host zabbix-hosts)]
+    (take 3 hosts)))
 
-(defn -main [& args]
-  (println "Hello from Rundeck Plugin CLI!"))
+;; For  testing purposes  read  properties  from a  file  and run  the
+;; query.  Eventually we  should format  the output  as Yaml/Json  for
+;; Rundeck to parse.
+(defn -main [path]
+  (let [properties (edn/read-string (slurp path))
+        zabbix-hosts (query properties)]
+    (pp/pprint zabbix-hosts)))
